8d7a558208c624685cc0bb67c5260333bb77815d
Young-Joon Park <yopark@sas.upenn.edu>
2022-09-10 15:28:52 -0700

    Add minus number logic to input text processing

--- app.js
+++ app.js
@@ -3,38 +3,65 @@ import {LinkedList} from './LinkedList.js'
 "use strict";
 
 const ALLOWED_OPERATORS = ['+', '-', '*', '/', '%', '^', '(', ')'];
-let operandList = new LinkedList();
-let operatorList = new LinkedList();
+const ALLOWED_START_OPERATORS = ['-', '('];
+const ALLOWED_END_OPERATORS = [')'];
+let operandStack = new LinkedList();
+let operatorStack = new LinkedList();
 
-
+function checkInput(inputString) {
+    let stringArray = inputString.split();
+}
 
 function processInput(inputString) {
     let stringArray = inputString.split();
     let tempNumber = "";
+    let minusNumber = false;
     for (i = 0; i < stringArray.length; i++) {
         let character = stringArray[i];
         if (!isNaN(character)) {
             tempNumber += character;
+        } else if (character === '(') {
+            if (stringArray[i + 1] === '-') {
+                minusNumber = true;
+            } else {
+                continue;
+            }
         } else if (ALLOWED_OPERATORS.includes(character)) {
             let numberToStack = +tempNumber;
-            addOperand(numberToStack);
+            if (minusNumber) {
+                addOperand(-numberToStack);
+                minusNumber = false;
+                popOperator();
+            } else { 
+                addOperand(numberToStack);
+            }
             addOperator(character);
             tempNumber = "";
+        }
+        if (i === stringArray.length - 1 && tempNumber !== "") {
+            let numberToStack = +tempNumber;
+            if (minusNumber) {
+                addOperand(-numberToStack);
+                minusNumber = false;
+                popOperator();
+            } else {
+                addOperand(numberToStack);
+            }
         }
     }
 }
 function addOperand(operand) {
-    operandList.push(operand);
+    operandStack.push(operand);
 }
 function addOperator(operator) {
-    operatorList.push(operator);
+    operatorStack.push(operator);
 }
 function popOperand() {
-    let operand = operandList.pop();
+    let operand = operandStack.pop();
     return operand;
 }
 function popOperator() {
-    let operator = operatorList.pop();
+    let operator = operatorStack.pop();
     return operator;
 }
 
